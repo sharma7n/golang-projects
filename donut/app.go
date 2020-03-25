68ff50c2446e2844493100a7e8f1d269100ab3b9
Nik <sharma7n@gmail.com>
2020-03-25 21:57:18 +0000

    get donuts from db

--- donut/app.go
+++ donut/app.go
@@ -1,50 +1,99 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/lib/pq"
+
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-
-	"../donut/lib"
-	"../donut/src"
 )
 
-func configureHTTPHandlers() {
-	http.HandleFunc("/", lib.Route(func() []byte {
-		return []byte("Hello, world!")
+// Shape ...
+type Shape int
+
+const (
+	// Ring ...
+	Ring Shape = 0
+
+	// Hole ...
+	Hole Shape = 1
+)
+
+// Donut ...
+type Donut struct {
+	Shape Shape
+}
+
+func configureHTTPHandlers(db *sql.DB) {
+	http.HandleFunc("/", route(func() ([]byte, error) {
+		return []byte("Hello, world!"), nil
 	}))
 
-	http.HandleFunc("/list", lib.Route(func() []byte {
-		donuts := []src.Donut{
-			src.Donut{
-				Shape: src.Ring,
-			},
-			src.Donut{
-				Shape: src.Hole,
-			},
-		}
-
-		response, err := json.Marshal(donuts)
+	http.HandleFunc("/list", route(func() ([]byte, error) {
+		donuts, err := getDonuts(db)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 
-		return response
+		return json.Marshal(donuts)
 	}))
 }
 
 func main() {
-	port := os.Args[1]
-	fmt.Println("Port:", port)
-	
-	// Serve on localhost
-	address := fmt.Sprintf(":%s", port)
+	// Program arguments
+	address := os.Args[1] // full host & port (e.g. "0.0.0.0:3000")
+	connStr := os.Args[2] // postgres connection string
 
-	configureHTTPHandlers()
+	// Configure the database connection using the given connection string
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	// Serve application
-	err := http.ListenAndServe(address, nil)
+	// Build the application using the database connection
+	configureHTTPHandlers(db)
+
+	// Serve the application using the given address
+	err = http.ListenAndServe(address, nil)
 	log.Fatal(err)
+}
+
+func getDonuts(db *sql.DB) (donuts []Donut, err error) {
+	rows, err := db.Query("SELECT * FROM Donut;")
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	
+	for rows.Next() {
+		var shape Shape
+		if err = rows.Scan(&shape); err != nil {
+			return
+		}
+		donut := Donut{Shape: shape}
+		donuts = append(donuts, donut)
+	}
+	
+	if err = rows.Err(); err != nil {
+		return
+	}
+
+	return
+}
+
+func route(logic func() ([]byte, error)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL)
+
+		response, err := logic()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Write(response)
+	}
 }
